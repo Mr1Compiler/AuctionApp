0ecab316075ac3bd4619e577844300d5d7af9e0b
mr1compiler <mr1compiler@gmail.com>
2025-04-12 16:27:36 +0300

    File Structure for Admin

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,10 @@
+/*
+This controller should include:
+
+dshboard() → shows admin overview.
+PendingAuctions() → list of unapproved auctions.
+Auctions() → all auctions (with filtering).
+Users() → manage users.
+Logs()
+*/
+
