c2cf72cd16a516251ac8b8ded332c08e7ae37890
Ayman H <aymen9255@gmail.com>
2025-05-01 22:42:51 +0300

    Solve the problem of admin log in as a user

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using AuctionApp.Areas.Identity.Data;
 using AuctionApp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,16 +9,35 @@ namespace AuctionApp.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly UserManager<AccountUser> _userManager;
 
-		public HomeController(ILogger<HomeController> logger)
+
+		public HomeController(ILogger<HomeController> logger, UserManager<AccountUser> userManager)
 		{
 			_logger = logger;
+			_userManager = userManager;
 		}
 
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
+			if (User.Identity.IsAuthenticated)
+			{
+				var user = await _userManager.GetUserAsync(User);
+				var roles = await _userManager.GetRolesAsync(user);
+
+				if (roles.Contains("Admin"))
+				{
+					return RedirectToAction("Dashboard", "Admin");
+				}
+				else
+				{
+					return RedirectToAction("Index", "User");
+				}
+			}
+
 			return View();
 		}
+
 
 		public IActionResult Privacy()
 		{
